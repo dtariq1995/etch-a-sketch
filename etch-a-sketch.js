4b6d2895caa75027a6f0a2f59629053fefc1463a
dtariq1995 <77370572+dtariq1995@users.noreply.github.com>
2022-01-01 12:30:05 -0600

    Added functionality to erase button to erase grid and then prompt user for new size for grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,3 +1,5 @@
+
+// create grid with user specified dimensions
 function createGrid() {
 
     let gridSize = prompt("Enter a number from 1 - 100 for the size of the grid");
@@ -21,19 +23,31 @@ function createGrid() {
         cell.style.height = `${size}px`;
         cell.style.width = `${size}px`;
     }
+    blackSketch();
+}
+
+function blackSketch() {
+    let gridCell = document.querySelectorAll('.cell');
+    gridCell.forEach((cell) => {
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = "black";
+        });
+    });
+}
+
+
+// remove cells from grid
+function eraseGrid() {
+
+    const grid = document.querySelectorAll('.cell');
+    grid.forEach((cell) => {
+        cell.remove();
+    });
+    createGrid();
 }
 
 
 
 createGrid();
-
-let gridCell = document.querySelectorAll('.cell');
-gridCell.forEach((cell) => {
-    cell.addEventListener('mouseover', () => {
-        cell.style.backgroundColor = "black";
-    });
-});
-
-    
 
 
